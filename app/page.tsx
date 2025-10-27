22162940615923041732da73f3e54cb50803fb7f
Asry18 <asryamaz000@gmail.com>
2025-10-27 13:58:58 +0530

    backen connected

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { use, useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/")
+      .then((res) => setMessage(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div>
-      Hello next world!!
+    <div style={{ padding: 40 }}>
+      <h1>Next.js + FastAPI Connection Test</h1>
+      <p>{message ? message : "Waiting for response..."}</p>
     </div>
-  )
+  );
 }
-
-export default page
